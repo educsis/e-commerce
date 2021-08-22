0779d555f1ba5c81171af1bc9c983968b019356b
Edwin Orellana <educsis@gmail.com>
2021-08-22 00:25:16 -0600

    adding my name

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import styles from '@styles/Home.module.css'
 
 export default function Home() {
   return (
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to Guatetiendas.com
         </h1>
 
         <p className={styles.description}>
